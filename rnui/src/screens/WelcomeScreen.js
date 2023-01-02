ffbf2ab6a7fa55b260cf2f9dc96bd66f7dd04a90
Justimiano Alves <jalves@Digitalkt.com>
2023-01-02 12:29:18 -0500

    multi language

--- rnui/src/screens/WelcomeScreen.js
+++ rnui/src/screens/WelcomeScreen.js
@@ -1,22 +1,29 @@
-import { useNavigation } from '@react-navigation/native';
-import '../localization/index';
+import {useNavigation} from '@react-navigation/native';
+
 import React from 'react';
-import { Button, Text, View } from 'react-native';
-import { useTranslation } from "react-i18next";
-import { utils } from '../libs/themes/theme';
-import { FORM_FIELDS } from '../navigation/routerNames';
+import {Button, Text, View} from 'react-native';
+
+import '../localization/index';
+import {useTranslation} from 'react-i18next';
+
+import {utils} from '../libs/themes/theme';
+import {FORM_FIELDS} from '../navigation/routerNames';
 
 const WelcomeScreen = () => {
+  const navigation = useNavigation();
+  const {t, i18n} = useTranslation();
 
-    const navigation = useNavigation();
-    const {t, i18n} = useTranslation();
-
-    return (
-        <View style={utils.rootScreen}>
-            <Text>{t('welcome')}</Text>
-            <Button onPress={() => { navigation.navigate(FORM_FIELDS) }} title="Go to Form Fields" />
-        </View>
-    );
-}
+  return (
+    <View style={utils.rootScreen}>
+      <Text>{t('welcome')}</Text>
+      <Button
+        onPress={() => {
+          navigation.navigate(FORM_FIELDS);
+        }}
+        title={t('continue')}
+      />
+    </View>
+  );
+};
 
 export default WelcomeScreen;
